df053fe9e4251bf13301fdc18f61526e938d45e0
Digital Experience <64876963+DigitalExperience-code@users.noreply.github.com>
2023-11-23 00:45:49 -0300

    Fix: Variaveis

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
-variable "project_name" {
-  description = "GCP Project name"
-  type        = string
-}
-#Aplicacao em outra regiao e zona
 variable "region" {
   description = "Google Cloud region"
   type        = string
-  default     = "us-west1"
+  default     = "us-east1"
 }
 
 variable "zone" {
   description = "Google Cloud zone"
   type        = string
-  default     = "us-west1-a"
+  default     = "us-east1-b"
+}
+
+variable "project_name" {
+  description = "GCP Project name"
+  type        = string
 }
